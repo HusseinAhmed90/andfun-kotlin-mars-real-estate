0f75734c42874ddf8e7cabebe22e6b0387f2bc51
HusseinAhmed90 <hussein.saeed90@outlook.com>
2021-05-02 14:51:19 +0200

    Adding api calling status

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -20,6 +20,7 @@ package com.example.android.marsrealestate.network
 // import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -45,7 +46,7 @@ private val retrofit = Retrofit.Builder()
         .build()
 
 /**
- * A public interface that exposes the [getProperties] method
+ * A public interface that exposes the [getPropertiesAsync] method
  */
 interface MarsApiService {
     /**
@@ -54,7 +55,7 @@ interface MarsApiService {
      * HTTP method
      */
     @GET("realestate")
-    suspend fun getProperties(): List<MarsProperty>
+    suspend fun getPropertiesAsync(): List<MarsProperty>
 }
 
 /**
